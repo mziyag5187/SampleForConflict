6cf9479299de01dbb67a390dcc1f1512fef4e48e
mziya <mziyag5187@gmail.com>
2022-08-17 22:20:03 +0300

    changes for conflict from Ziya branch

---
+++ src/test/java/com/cydeo/Conflict.java
@@ -0,0 +1,18 @@
+package com.cydeo;
+
+public class Conflict {
+    public static void main(String[] args) {
+        // changes from Ziya branch
+        // changes from Ziya branch
+        // changes from Ziya branch
+        // changes from Ziya branch
+        // changes from Ziya branch
+        // changes from Ziya branch
+        // changes from Ziya branch
+        // changes from Ziya branch
+        // changes from Ziya branch
+        // changes from Ziya branch
+        // changes from Ziya branch
+
+    }
+}
