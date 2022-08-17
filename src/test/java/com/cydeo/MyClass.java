b5b93968fc508600f20e473de45110b6a4a9f254
mziya <mziyag5187@gmail.com>
2022-08-17 15:06:45 +0300

    first changes made on master

---
+++ src/test/java/com/cydeo/MyClass.java
@@ -0,0 +1,11 @@
+package com.cydeo;
+
+public class MyClass {
+    public static void main(String[] args) {
+        //changes from master
+        //changes from master
+        //changes from master
+        //changes from master
+
+    }
+}
