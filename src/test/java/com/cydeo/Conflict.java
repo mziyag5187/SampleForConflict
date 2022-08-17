5f9110c2738b967741c8dad18e5d43bd69a9496e
mziya <62723491+mziyag5187@users.noreply.github.com>
2022-08-17 22:17:38 +0300

    Create Conflict.java

---
+++ src/test/java/com/cydeo/Conflict.java
@@ -0,0 +1,11 @@
+package com.cydeo;
+
+public class Conflict {
+    public static void main(String[] args) {
+        //changes from remote master
+        //changes from remote master
+        //changes from remote master
+        //changes from remote master
+
+    }
+}
